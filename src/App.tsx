f52434871b32760137d8c25db0e708352f1161a6
听孙燕姿的Jay <1928999500@qq.com>
2024-07-17 20:11:56 +0800

    feat: dynamic load component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { useEffect } from "react";
 import Header from "./components/header";
-// import "./locales/zh.CN";
+
+const load = async (params: string) => {
+  const modules = import.meta.glob("./locales/*.ts");
+  const loadModule = modules[`./locales/${params}.ts`];
+  if (loadModule) {
+    const module = await loadModule();
+    return (module as { data: string }).data;
+  } else {
+    throw new Error(`Module './locales/${params}.ts' not found`);
+  }
+};
 
 function App() {
   useEffect(() => {
-    // const importModule = (m: string) => import(`./locales/${m}.ts`);
-    // importModule("zh_CN");
+    load("zh_CN").then((data) => {
+      console.log(data);
+    });
   }, []);
   return (
     <>
